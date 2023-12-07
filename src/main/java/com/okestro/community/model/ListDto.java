c4423afb397ce08bcb467ee6551c77853757712f
parkyoungsun <ys.park@okestro.com>
2023-12-07 17:52:25 +0900

    2023.12.07 Backend

--- src/main/java/com/okestro/community/model/ListDto.java
+++ src/main/java/com/okestro/community/model/ListDto.java
@@ -1,9 +1,7 @@
 package com.okestro.community.model;
 
-import lombok.Getter;
 import lombok.Setter;
 
-@Getter
 @Setter
 public class ListDto {
 
@@ -11,34 +9,14 @@ public class ListDto {
     private String title;
     private String name;
     private String content;
+    //    public void setCommentCount(int commentCount) {
+    //        this.commentCount = commentCount;
+    //    }
     //private int commentCount;
     private  String commentCount;
 //    private String[] comment; // 추후 구현
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-//    public void setCommentCount(int commentCount) {
-//        this.commentCount = commentCount;
-//    }
-    public void setCommentCount(String commentCount) {
-        this.commentCount = commentCount;
-    }
-
-//    public void setComment(String[] comment) {
+    //    public void setComment(String[] comment) {
 //        this.comment = comment;
 //    } // 추후 구현
 }
