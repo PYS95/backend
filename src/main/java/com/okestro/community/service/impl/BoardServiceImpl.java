45bb03bb3ee38029668aad4e17a4cbbabe64aac3
parkyoungsun <ys.park@okestro.com>
2023-12-18 13:01:13 +0900

    2023.12.07 Backend

--- src/main/java/com/okestro/community/service/impl/BoardServiceImpl.java
+++ src/main/java/com/okestro/community/service/impl/BoardServiceImpl.java
@@ -10,8 +10,6 @@ import java.util.List;
 @Service
 public class BoardServiceImpl implements BoardService {
 
-    private String name = "ys";
-
     private final List<ListDto> listDtoMember = new ArrayList<>();
 
     @Override
