e4b910a3f5ccac0b186231fdb88b47d56eb2ca32
jh.jeong <jh.jeong@okestro.com>
2023-12-20 10:18:17 +0900

    2023.12.20 | BoardServiceImpl 객체수정 (BoardController, ListDto도 수정)

--- src/main/java/com/okestro/community/model/ListDto.java
+++ src/main/java/com/okestro/community/model/ListDto.java
@@ -1,20 +1,26 @@
 package com.okestro.community.model;
-
 import lombok.Data;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
 import java.util.List;
 
-@Setter
 @Data
+@NoArgsConstructor
 public class ListDto {
 
     private Long id;
     private String title;
     private String name;
     private String content;
-    private List<String> comments = new ArrayList<>();
+    private List<String> comments;
     private int commentCount;
 
+    public ListDto(Long id, String title, String name, String content, List<String> comments, int commentCount) {
+        this.id = id;
+        this.title = title;
+        this.name = name;
+        this.content = content;
+        this.comments = comments;
+        this.commentCount = commentCount;
+    }
 }
