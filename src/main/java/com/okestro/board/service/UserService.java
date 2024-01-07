90fb11e091bc0fa8fb6b58a30935bb395bd991ff
jh.jeong <jh.jeong@okestro.com>
2024-01-07 23:39:41 +0900

    2024.01.0 | 회원정보 수정,취소, 삭제 구현 및 목록이나 취소 버튼클릭했을 시 화면 수정

--- src/main/java/com/okestro/board/service/UserService.java
+++ src/main/java/com/okestro/board/service/UserService.java
@@ -1,7 +1,16 @@
 package com.okestro.board.service;
 
+import com.okestro.board.model.UserDto;
+
 public interface UserService {
+
     boolean isUsernameUnique(String userId);
 
     void registerUser(String userId, String userPw);
+
+    UserDto getUser(String userId);
+
+    boolean updateUserPassword(String userId, String userPw);
+
+    boolean deleteUser(String userId);
 }
